6223b8c8512ed92e207e26eeeec1fbf741fbdb6c
Mahmut <139115718+1MAM2@users.noreply.github.com>
2025-10-04 02:04:34 +0300

    EmailConfirm_2

--- src/Pages/ConfirmEmail.tsx
+++ src/Pages/ConfirmEmail.tsx
@@ -1,33 +1,31 @@
-import React, { useEffect, useState } from 'react'
-import { useSearchParams } from 'react-router-dom'
+import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
 
 const ConfirmEmail = () => {
-    const[searchParams] = useSearchParams();
-    const[status,setStatus] = useState("Confirm waiting...");
+  const [searchParams] = useSearchParams();
+  const [status, setStatus] = useState("Confirm waiting...");
 
+  useEffect(() => {
+    const token = searchParams.get("token");
+    const email = searchParams.get("email");
 
-    useEffect(()=>{
-     const token = searchParams.get("token");
-     const email = searchParams.get("email");
-     
-     if (token && email) {
-        fetch(`https://asp-net-web-api-ym61.onrender.com/api/users/confirm-email?email=${email}&token=${encodeURIComponent(token)}`,{
-           method : "POST" 
-        })
-        .then(res => res.json())
-        .then(data => setStatus("Email is confirmed"))
-        .catch(err => setStatus("An Error Accured"));
-     }
-     else
-     {
-        setStatus("Invalid link");
-     }
-    },[]);
-  return (
-    <div>
-      {status}
-    </div>
-  )
-}
+    if (token && email) {
+      fetch(
+        `https://asp-net-web-api-ym61.onrender.com/api/users/confirm-email?email=${email}&token=${encodeURIComponent(
+          token
+        )}`,
+        {
+          method: "POST",
+        }
+      )
+        .then((res) => res.json())
+        .then(() => setStatus("Email is confirmed"))
+        .catch(() => setStatus("An Error Accured"));
+    } else {
+      setStatus("Invalid link");
+    }
+  }, []);
+  return <div>{status}</div>;
+};
 
-export default ConfirmEmail
+export default ConfirmEmail;
